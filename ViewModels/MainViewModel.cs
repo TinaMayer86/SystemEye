961c4ca2177acacf54fcac53e6a397115965564f
Tina-Mayer86 <tinarina861@gmail.com>
2026-04-24 11:28:38 +0200

    WIP - Von MySQL zu SqLite gewechselt, Graphen funktionieren jetzt, UML erweitert

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text.Json; // Hinzugefügt für das Laden der Sensor-Config
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
